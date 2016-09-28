c919f3a2254dfe9190b2d5d94fc53b2945f22f45
Serg T <sergtsaeb@gmail.com>
2016-09-28 14:58:11 -0700

    create 3 functions to specifications

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -14,12 +14,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    
-    
-    
-    
     // Implement your functions here!
+    func averageIsAbove75(_ num1: Double,_ num2: Double,_ num3: Double) -> Bool {
+        let numTot = num1 + num2 + num3
+        let numAvg = numTot / 3
+        
+        
+        if numAvg > 75 {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    
+    func passwordCombo(username: String, password: Int) -> String {
+        let arrayOfNames = ["Jerry", "Elaine", "Michael"]
+        
+        if arrayOfNames.contains(username) && (password % 3 ) == 0 {
+            return("Welcome")
+        } else {
+            return("Access Denied")
+        }
+        
+    }
+    
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return ("Kiss")
+            
+        case "🐈":
+            return ("Cat")
+            
+        case "🐢":
+            return ("Turtle")
+            
+        case "🍕":
+            return ("Pizza")
+            
+        case "👻":
+        return ("Ghost")
+            
+        default:
+            return ("Unknown")
+            
+    }
+    }
     
     
     
